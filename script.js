8decc33b02b3f1c6d6786c1c72551c68f43663d6
manikkaushal <manik.kaushal1@gmail.com>
2023-02-02 17:53:06 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+function display(val){
+
+    document.getElementById('result').value += val
+
+    return val
+
+}
+display();
+
+function solve(){
+
+    let x = document.getElementById('result').value
+
+    let y = eval(x);
+
+    document.getElementById('result').value = y
+
+    return y
+
+}
+
+solve();
+
+
+function clearScreen(){
+
+    document.getElementById('result').value = ''
+
+}
+clearScreen();
